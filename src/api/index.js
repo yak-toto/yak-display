2a3f280513b422300f22b5f04361f6e08d25873c
clarisse <gui.lepape25@gmail.com>
2023-03-26 23:10:54 +0200

    Refactoring bearer token encoding

--- src/api/index.js
+++ src/api/index.js
@@ -7,6 +7,10 @@ const GLOBAL_ENDPOINT = 'api';
 const VERSION = 'v1';
 const BASE_URL = `${URL}/${GLOBAL_ENDPOINT}/${VERSION}`;
 
+function encodeBearerToken(jwtToken) {
+  return `Bearer ${jwtToken}`;
+}
+
 const api = {
   // ------------------------------
   // Auth interface
@@ -22,43 +26,54 @@ const api = {
   // ------------------------------
   getGroupNames(phaseName, jwt) {
     return axios.get(`${BASE_URL}/groups/phases/${phaseName}`, {
-      headers: { Authorization: `Bearer ${jwt}` },
+      headers: { Authorization: encodeBearerToken(jwt) },
     });
   },
   getGroup(groupName, jwt) {
-    return axios.get(`${BASE_URL}/bets/groups/${groupName}`, { headers: { Authorization: `Bearer ${jwt}` } });
+    return axios.get(
+      `${BASE_URL}/bets/groups/${groupName}`,
+      { headers: { Authorization: encodeBearerToken(jwt) } },
+    );
   },
   getGroupResult(groupName, jwt) {
     return axios.get(`${BASE_URL}/bets/groups/rank/${groupName}`, {
-      headers: { Authorization: `Bearer ${jwt}` },
+      headers: { Authorization: encodeBearerToken(jwt) },
     });
   },
   postBetsFinalePhase(jwt) {
-    return axios.post(`${BASE_URL}/bets/finale_phase`, null, { headers: { Authorization: `Bearer ${jwt}` } });
+    return axios.post(
+      `${BASE_URL}/bets/finale_phase`,
+      null,
+      { headers: { Authorization: encodeBearerToken(jwt) } }
+    );
   },
   putBetsByPhase(phaseCode, bets, jwt) {
     return axios.put(`${BASE_URL}/binary_bets/phases/${phaseCode}`, bets, {
-      headers: { Authorization: `Bearer ${jwt}` },
+      headers: { Authorization: encodeBearerToken(jwt) },
     });
   },
   patchScoreBet(betId, score1, score2, jwt) {
     return axios.patch(
       `${BASE_URL}/score_bets/${betId}`,
       { team1: { score: score1 }, team2: { score: score2 } },
-      { headers: { Authorization: `Bearer ${jwt}` } },
+      { headers: { Authorization: encodeBearerToken(jwt) } },
     );
   },
   // ------------------------------
   // Result interface
   // ------------------------------
   getScoreBoard(jwt) {
-    return axios.get(`${BASE_URL}/score_board`, { headers: { Authorization: `Bearer ${jwt}` } });
+    return axios.get(`${BASE_URL}/score_board`, { headers: { Authorization: encodeBearerToken(jwt) } });
   },
   // ------------------------------
   // Admin interface
   // ------------------------------
   postComputePoints(jwt) {
-    return axios.post(`${BASE_URL}/compute_points`, null, { headers: { Authorization: `Bearer ${jwt}` } });
+    return axios.post(
+      `${BASE_URL}/compute_points`,
+      null,
+      { headers: { Authorization: encodeBearerToken(jwt) } }
+    );
   },
 };
 
