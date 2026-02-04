9d85d2be7a4e3cf63786e088554236406295ea2c
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-02-04 17:48:32 +0100

    Improve login page UI

---
+++ src/components/icons/SpinnerIcon.vue
@@ -0,0 +1,23 @@
+<template>
+  <span class="spinner"></span>
+</template>
+
+<style scoped>
+.spinner {
+  display: inline-block;
+  width: 14px;
+  height: 14px;
+  border: 2px solid rgba(255, 255, 255, 0.3);
+  border-top-color: white;
+  border-radius: 50%;
+  animation: spin 0.8s linear infinite;
+  margin-right: 0.5rem;
+  vertical-align: middle;
+}
+
+@keyframes spin {
+  to {
+    transform: rotate(360deg);
+  }
+}
+</style>
