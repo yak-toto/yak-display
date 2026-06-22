ecfae81127c065152260e59cf6f7aeb7da50b682
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-06-22 09:03:06 +0200

    Add page title to uniformize all pages display

---
+++ src/components/PageTitle.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1 class="page-title">{{ title }}</h1>
+</template>
+
+<script setup lang="ts">
+defineProps<{ title: string }>();
+</script>
+
+<style scoped>
+.page-title {
+  font-size: 2rem;
+  font-weight: 700;
+  color: var(--color-text);
+}
+</style>
