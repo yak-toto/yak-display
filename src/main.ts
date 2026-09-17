49b1e50437ea4e0432b47841a5843369ef3b4b3d
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-09-17 19:48:26 +0200

    Self host fonts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,7 @@
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
 import { createPinia } from 'pinia';
 import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import { createApp } from 'vue';
